45274852e8e596f57efa4012b0d3f263d65fc0b7
Ganesh Tiwari <Gat786@outlook.com>
2018-08-30 23:42:25 +0530

    python completed with bugs

--- vernam/python/vernam_cipher.py
+++ vernam/python/vernam_cipher.py
@@ -8,8 +8,8 @@ ciphers="0123456789"+string.ascii_lowercase+string.ascii_uppercase
 def get_key(text_input):
     text_key=""
     for every in text_input:
-        num=random.randint(0,len(ciphers))
-        text_key +=ciphers[num]
+        num=random.randint(0,len(ciphers)-1)
+        text_key += ciphers[num]
     return text_key
 
 def get_index(input_text):
@@ -21,30 +21,41 @@ def get_index(input_text):
 def get_list(text):
     return [i for i in text]
 
-text_list=get_list(text_input)
-cipher_list=get_list(get_key(text_input))
-
-print(text_list)
-print(cipher_list)
-
-index_message=get_index(text_list)
-index_ciphertext=get_index(cipher_list)
-
-print(index_message)
-
-print(index_ciphertext)
-
-answer_list=[]
-
 def get_charachter(list_numbers):
     list_charachters=[]
     for i in list_numbers:
         list_charachters.append(ciphers[i])
     return list_charachters
 
-for a in range(0,len(index_message)):
-    answer_list.append(index_message[a]^index_ciphertext[a])
-print(answer_list)
 
-print(get_charachter(answer_list))
+def create_vernam_cipher_clone(index_message,index_ciphertext):
+    answer_list=[]
+    for a in range(0,len(index_message)):
+        answer_list.append(index_message[a]^index_ciphertext[a])
+    return answer_list
 
+
+def vernam_cipher_master(text_input,key_generated):
+    list_index_text = get_index(text_input)
+    list_index_key = get_index(key_generated)
+
+    print("list of numbers of text is ")
+    print(list_index_text)
+
+    print("list of numbers of key is ")
+    print(list_index_key)
+
+    encrypted = create_vernam_cipher_clone(list_index_text,list_index_key)
+    print("raw encrypted is ")
+    print(encrypted)
+    print("cipher text is ")
+    print(get_charachter( encrypted))
+
+    decrypted = create_vernam_cipher_clone(encrypted,list_index_key)
+    print("raw Decrypted is ")
+    print(decrypted)
+    print("Decrypted is ")
+    print(get_charachter(decrypted))
+
+
+vernam_cipher_master(text_input,get_key(text_input))
